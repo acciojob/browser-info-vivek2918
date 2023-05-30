00565cf4e12603c74f0473c4a9191ca612728a64
Vivek Chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-05-30 09:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const browserName = navigator.appName;
+const version = navigator.appVersion;
+document.getElementById('browser-info').innerText = "You are using " + browserName + " version " + version;
